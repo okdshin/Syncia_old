a1c4522d3246e27708c69f5513565801986389aa
s.okada <kokuzen@gmail.com>
2012-11-03 16:30:44 +0900

    Added SynciaCore.o3.mk.

---
+++ SynciaCore.o3.mk
@@ -0,0 +1,18 @@
+CXX = g++ -std=gnu++0x -O3
+CXXFLAGS = -D SYNCIACORE_UNIT_TEST
+INCLUDES = 
+LIBS = -lboost_filesystem -lboost_date_time -lboost_iostreams -lboost_serialization -lcrypto -lboost_thread -ldl -lpthread -lboost_system
+OBJS = SynciaCore.o
+PROGRAM = SynciaCore.out
+
+all:$(PROGRAM)
+
+$(PROGRAM): $(OBJS)
+	$(CXX) $(CXXFLAGS) $^ $(INCLUDES) $(LIBS) -o $(PROGRAM)
+
+.cpp.o:
+	$(CXX) $(CXXFLAGS) $(INCLUDES) $(LIBS) -c $<
+
+.PHONY: clean
+clean:
+	rm -f *o $(PROGRAM)
